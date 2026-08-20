5eaff8f303d18cb05dd35b53a5fa01fe46f7bb3d
anntrofim-debug <ann.trofim@gmail.com>
2026-08-20 12:51:05 +0200

    Add Blog Section
    Closes #14

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,12 +6,18 @@ import {
 import AppLayout from "./components/layouts/AppLayout";
 import ErrorElement from "./components/feedbackElements/ErrorElement"
 import Home from "./pages/Home"
+import { homeLoader } from "./components/loaders/DataLoader";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<AppLayout />}>
-        <Route index element={<Home />} errorElement={<ErrorElement />} />
+        <Route
+          index
+          element={<Home />}
+          loader={homeLoader}
+          errorElement={<ErrorElement />}
+        />
       </Route>
     </Route>,
   ),
